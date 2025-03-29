590a960bb8713d3762e780255cb5d940182c99c4
KOfW <108273260+KOfW@users.noreply.github.com>
2025-03-29 15:09:20 +0700

    Thêm dự án vào github

---
+++ BookStore/src/test/java/com/nlu/cdweb/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlu.cdweb.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
